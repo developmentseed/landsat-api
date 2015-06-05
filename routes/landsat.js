12124ddfeac3cf0518a6d45fe4e7f9d940a8c6f7
Scisco <scisco7@gmail.com>
2015-06-05 12:43:36 -0400

    add support for POST

--- routes/landsat.js
+++ routes/landsat.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var Boom = require('boom');
 var landsat = require('../controllers/landsat.js');
 
@@ -10,8 +11,25 @@ module.exports = [
     handler: function (request, reply) {
       var params = {};
 
+      // For GET
       if (request.query) {
         params = request.query;
+      }
+
+      // For POST
+      if (request.payload) {
+        params = request.payload;
+
+        //Crude hack for pagination lack of support for POST
+        if (_.has(request.payload, 'page')) {
+          request.page = _.parseInt(request.payload.page);
+          request.payload = _.omit(request.payload, 'page');
+        }
+
+        if (_.has(request.payload, 'limit')) {
+          request.limit = _.parseInt(request.payload.limit);
+          request.payload = _.omit(request.payload, 'limit');
+        }
       }
 
       landsat(params, request, function (err, records, count) {
