cf674cb36e91f8a33dfba631a9eb59991c3aa59c
Scisco <scisco7@gmail.com>
2015-06-12 14:09:53 -0400

    increase default timeout

--- services/elasticsearch.js
+++ services/elasticsearch.js
@@ -6,5 +6,5 @@ module.exports = new elasticsearch.Client({
   host: process.env.ES_HOST || 'localhost:9200',
 
   // Note that this doesn't abort the query.
-  requestTimeout: 5000  // milliseconds
+  requestTimeout: 10000  // milliseconds
 });
