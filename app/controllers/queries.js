04835a2d7120699f4aa35592a7f46344940845e2
Scisco <scisco7@gmail.com>
2015-06-16 15:33:03 -0400

    full documentation

--- app/controllers/queries.js
+++ app/controllers/queries.js
@@ -5,6 +5,11 @@ var _ = require('lodash');
 var gjv = require('geojson-validation');
 var err = require('./errors.js')
 
+/**
+ * @apiDefine search
+ * @apiParam {string} [search] Supports Lucene search syntax for all available fields
+ * in the landsat meta data. <br> If search is used, all other parameters are ignored.
+**/
 var legacyParams = function (params, q, limit) {
   var supported_query_re = new RegExp('^[0-9a-zA-Z#\*\.\_\:\(\)\"\\[\\]\{\}\\-\\+\>\<\= ]+$');
 
@@ -29,6 +34,14 @@ var geojsonQueryBuilder = function (feature, query) {
   return query;
 }
 
+/**
+ * @apiDefine contains
+ * @apiParam {string} [contains] Evaluates whether the given point is within the
+ * bounding box of a landsat image.
+ *
+ * Accepts `latitude` and `longitude`. They have to be separated by a `,`
+ * with no spaces in between. Example: `contains=23,21`
+**/
 var contains = function (params, query) {
   var correct_query = new RegExp('^[0-9\.\,\-]+$');
   if (correct_query.test(params)) {
@@ -46,6 +59,13 @@ var contains = function (params, query) {
   }
 };
 
+/**
+ * @apiDefine intersects
+ * @apiParam {string/geojson} [intersects] Evaluates whether the give geojson is intersects
+ * with any landsat images.
+ *
+ * Accepts valid geojson.
+**/
 var intersects = function (params, query) {
   try {
     var geojson = JSON.parse(params);
@@ -69,6 +89,42 @@ var intersects = function (params, query) {
   }
 };
 
+/**
+ * @apiDefine rangeFields
+ * @apiParam {string} [date_from] The lower limit for `acquisitionDate` field.
+ * Returns all records with `acquisitionDate` after this date. <br>
+ * Accepted format: `YYYY-MM-DD`.
+ *
+ * @apiParam {string} [date_to] The upper limit for `acquisitionDate` field.
+ * Returns all records with `acquisitionDate` before this date. <br>
+ * Accepted format: `YYYY-MM-DD`.
+ *
+ * @apiParam {string} [scene_start_time_from] The lower limit for `sceneStartTime` field.
+ * Returns all records with `sceneStartTime` after this date. <br>
+ * Accepted format: `YYYY-MM-DD`.
+ *
+ * @apiParam {string} [scene_start_time_to] The upper limit for `sceneStartTime` field.
+ * Returns all records with `sceneStartTime` before this date. <br>
+ * Accepted format: `YYYY-MM-DD` or `YYYY-MM-DD HH:mm:ss.ZZZ`.
+ *
+ * @apiParam {number} [cloud_from] The lower limit for `cloudCoverFull` field.
+ * Returns all records with `cloudCoverFull` greater than this value.
+ *
+ * @apiParam {number} [cloud_to] The upper limit for `cloudCoverFull` field.
+ * Returns all records with `cloudCoverFull` smaller than this value.
+ *
+ * @apiParam {number} [sun_azimuth_from] The lower limit for `sunAzimuth` field.
+ * Returns all records with `sunAzimuth` greater than this value.
+ *
+ * @apiParam {number} [sun_azimuth_to] The upper limit for `sunAzimuth` field.
+ * Returns all records with `sunAzimuth` smaller than this value.
+ *
+ * @apiParam {number} [sun_elevation_from] The lower limit for `sunElevation` field.
+ * Returns all records with `sunElevation` greater than this value.
+ *
+ * @apiParam {number} [sun_elevation_to] The upper limit for `sunElevation` field.
+ * Returns all records with `sunElevation` smaller than this value.
+**/
 var rangeQuery = function (from, to, field, query) {
 
   if (from && to) {
@@ -84,6 +140,16 @@ var rangeQuery = function (from, to, field, query) {
   }
 };
 
+/**
+ * @apiDefine termFields
+ * @apiParam {string} [scene_id] Performs exact search on `sceneID` field.
+ * @apiParam {number} [row] Performs exact search on `row` field.
+ * @apiParam {number} [path] Performs exact search on `path` field.
+ * @apiParam {string} [sensor] Performs exact search on `sensor` field.
+ * @apiParam {string} [receiving_station] Performs exact search on `receivingStation` field.
+ * @apiParam {string="day","night"} [day_or_night=night] Performs exact search on `dayOrNight` field.
+ *
+**/
 var termQuery = function (param, field, query) {
   return query.must(ejs.TermQuery(field, param));
 };
--- app/routes/landsat.js
+++ app/routes/landsat.js
@@ -6,82 +6,47 @@ var landsat = require('../controllers/landsat.js');
 
 
 /**
- * @api {get} /landsat Landsat Meta Data Search
+ * @api {get} /landsat GET
  * @apiName getLandsatMeta
- * @apiGroup landsat
+ * @apiGroup API
  * @apiVersion 1.0.0
  *
  * @apiDescription This is the only end-point of landsat-api. Through this end-point
  * you can search and query landsat meta data. It supports both `GET` and `POST` requests.
  *
- * @apiParam {string} [contains] Evaluates whether the given point is within the
- * bounding box of a landsat image.
- *
- * Accepts `latitude` and `longitude`. They have to be separated by a `,`
- * with no spaces in between. Example: `contains=23,21`
- *
- * @apiParam {string/geojson} [intersects] Evaluates whether the give geojson is intersects
- * with any landsat images.
- *
- * Accepts valid geojson.
- *
- * @apiParam {string} [date_from] The lower limit for `acquisitionDate` field.
- * Returns all records with `acquisitionDate` after this date. <br>
- * Accepted format: `YYYY-MM-DD`.
- *
- * @apiParam {string} [date_to] The upper limit for `acquisitionDate` field.
- * Returns all records with `acquisitionDate` before this date. <br>
- * Accepted format: `YYYY-MM-DD`.
- *
- * @apiParam {string} [scene_start_time_from] The lower limit for `sceneStartTime` field.
- * Returns all records with `sceneStartTime` after this date. <br>
- * Accepted format: `YYYY-MM-DD`.
- *
- * @apiParam {string} [scene_start_time_to] The upper limit for `sceneStartTime` field.
- * Returns all records with `sceneStartTime` before this date. <br>
- * Accepted format: `YYYY-MM-DD` or `YYYY-MM-DD HH:mm:ss.ZZZ`.
- *
- * @apiParam {number} [cloud_from] The lower limit for `cloudCoverFull` field.
- * Returns all records with `cloudCoverFull` greater than this value.
- *
- * @apiParam {number} [cloud_to] The upper limit for `cloudCoverFull` field.
- * Returns all records with `cloudCoverFull` smaller than this value.
- *
- * @apiParam {number} [sun_azimuth_from] The lower limit for `sunAzimuth` field.
- * Returns all records with `sunAzimuth` greater than this value.
- *
- * @apiParam {number} [sun_azimuth_to] The upper limit for `sunAzimuth` field.
- * Returns all records with `sunAzimuth` smaller than this value.
- *
- * @apiParam {number} [sun_elevation_from] The lower limit for `sunElevation` field.
- * Returns all records with `sunElevation` greater than this value.
- *
- * @apiParam {number} [sun_elevation_to] The upper limit for `sunElevation` field.
- * Returns all records with `sunElevation` smaller than this value.
- *
- * @apiParam {string} [scene_id] Performs exact search on `sceneID` field.
- *
- * @apiParam {number} [row] Performs exact search on `row` field.
- *
- * @apiParam {number} [path] Performs exact search on `path` field.
- *
- * @apiParam {string} [sensor] Performs exact search on `sensor` field.
- *
- * @apiParam {string} [receiving_station] Performs exact search on `receivingStation` field.
- *
- * @apiParam {string="day","night"} [day_or_night=night] Performs exact search on `dayOrNight` field.
- *
  * @apiParam {number} [limit=1] Limit the results.
- *
  * @apiParam {number} [page=1] Paginate through results.
- *
  * @apiParam {number} [skip] Number of records to skip.
+ * @apiUse contains
+ * @apiUse intersects
+ * @apiUse rangeFields
+ * @apiUse termFields
+ * @apiUse search
  *
- * @apiParam {string} [search] Supports Lucene search syntax for all available fields
- * in the landsat meta data. <br> If search is used, all other parameters are ignored.
+ * @apiUse landsatSuccess
  *
- * @apiSuccess {string} firstname Firstname of the User.
- * @apiSuccess {string} lastname  Lastname of the User.
+ * @apiError statusCode     The error code.
+ * @apiError error          Error Name.
+ * @apiError message        Error Message.
+ * @apiErrorExample {json} Error-Response:
+ *     HTTP/1.1 400 Bad Request
+ *     {
+ *      "statusCode": 400,
+ *      "error": "Bad Request",
+ *      "message": "Incorrect coordinates: 38.9117833,^77.0298486. Only digits, dot, minus and comma are allowd."
+ *     }
+ */
+
+ /**
+ * @api {post} /landsat POST
+ * @apiName postLandsatMeta
+ * @apiGroup API
+ * @apiVersion 1.0.0
+ *
+ * @apiDescription This is the only end-point of landsat-api. Through this end-point
+ * you can search and query landsat meta data. It supports both `GET` and `POST` requests.
+ *
+ * All parameters and response is the same as the `GET` request.
  */
 module.exports = [
   {
