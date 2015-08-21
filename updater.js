d4230b97cc679aed94283219b9eb5ff0f6d2d48d
Scisco <scisco7@gmail.com>
2015-08-21 15:52:49 -0400

    switch updater to mongodb

--- updater.js
+++ updater.js
@@ -1,12 +1,25 @@
 'use strict';
+require('envloader').load();
 var Updater = require('landsat-meta-updater');
+var mongoose = require('mongoose');
 
-var u = new Updater('landsat', '8', 500);
+console.log(process.env.MONGODB_URL);
 
-// Update Elastic Search
-u.updateEs(function (err, msg) {
-  if (err) {
-    console.log('Error:', err);
-  }
-  console.log(msg);
+var dbUrl = process.env.MONGODB_URL || 'mongodb://localhost/landsat-api';
+console.log(dbUrl);
+mongoose.connect(dbUrl);
+var db = mongoose.connection;
+
+var u = new Updater('landsat', '8', 1000);
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('connected');
+  u.updateMongoDb(dbUrl, function (err, msg) {
+    if (err) {
+      console.log('Error:', err);
+    }
+    console.log(msg);
+    process.exit();
+  });
 });
