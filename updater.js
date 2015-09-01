f5f6636a34c02bc9fd332c0bd679dec1a50abd00
Scisco <scisco7@gmail.com>
2015-09-01 16:00:31 -0400

    update using tmp directory

--- updater.js
+++ updater.js
@@ -2,12 +2,16 @@
 require('envloader').load();
 var Updater = require('landsat-meta-updater');
 var mongoose = require('mongoose');
+var tmp = require('tmp');
 
 var dbUrl = process.env.MONGODB_URL || 'mongodb://localhost/landsat-api';
 mongoose.connect(dbUrl);
 var db = mongoose.connection;
 
-var u = new Updater('landsat', '8', 1000);
+// Temp folder
+var tmpobj = tmp.dirSync();
+
+var u = new Updater('landsat', '8', 1000, tmpobj);
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -17,6 +21,7 @@ db.once('open', function () {
       console.log('Error:', err);
     }
     console.log(msg);
+    tmpobj.removeCallback();
     process.exit();
   });
 });
