ebc170fc94a529b317c07cde73da3b95188cdd71
Scisco <scisco7@gmail.com>
2015-09-02 10:02:30 -0400

    move repeated lines to a function

--- app/routes/landsat.js
+++ app/routes/landsat.js
@@ -63,28 +63,7 @@ module.exports = [
         landsat = esList;
       }
 
-      var params = {};
-
-      // For GET
-      if (request.query) {
-        params = request.query;
-      }
-
-      // For POST
-      if (request.payload) {
-        params = request.payload;
-
-        // Crude hack for pagination lack of support for POST
-        if (_.has(request.payload, 'page')) {
-          request.page = _.parseInt(request.payload.page);
-          request.payload = _.omit(request.payload, 'page');
-        }
-
-        if (_.has(request.payload, 'limit')) {
-          request.limit = _.parseInt(request.payload.limit);
-          request.payload = _.omit(request.payload, 'limit');
-        }
-      }
+      var params = prepareRequest(request);
 
       // Send for processing
       landsat(params, request, function (err, records, count) {
@@ -110,28 +89,7 @@ module.exports = [
         landsat = esCount;
       }
 
-      var params = {};
-
-      // For GET
-      if (request.query) {
-        params = request.query;
-      }
-
-      // For POST
-      if (request.payload) {
-        params = request.payload;
-
-        // Crude hack for pagination lack of support for POST
-        if (_.has(request.payload, 'page')) {
-          request.page = _.parseInt(request.payload.page);
-          request.payload = _.omit(request.payload, 'page');
-        }
-
-        if (_.has(request.payload, 'limit')) {
-          request.limit = _.parseInt(request.payload.limit);
-          request.payload = _.omit(request.payload, 'limit');
-        }
-      }
+      var params = prepareRequest(request);
 
       // Send for processing
       landsat(params, request, function (err, count) {
@@ -153,3 +111,30 @@ module.exports = [
   },
 
 ];
+
+var prepareRequest = function (request) {
+  var params = {};
+
+  // For GET
+  if (request.query) {
+    params = request.query;
+  }
+
+  // For POST
+  if (request.payload) {
+    params = request.payload;
+
+    // Crude hack for pagination lack of support for POST
+    if (_.has(request.payload, 'page')) {
+      request.page = _.parseInt(request.payload.page);
+      request.payload = _.omit(request.payload, 'page');
+    }
+
+    if (_.has(request.payload, 'limit')) {
+      request.limit = _.parseInt(request.payload.limit);
+      request.payload = _.omit(request.payload, 'limit');
+    }
+  }
+
+  return params
+};
