de018b38e05da6caa9494444743f832e25ec8e6f
Scisco <scisco7@gmail.com>
2015-09-02 12:20:52 -0400

    area measurement func

--- app/libs/shared.js
+++ app/libs/shared.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var err = require('./errors.js')
-
+var err = require('./errors.js');
+var turfArea = require('turf-area');
+var validator = require('validator');
 
 var parseGeoJson = function (geojson) {
   // if we receive an object, assume it's GeoJSON, if not, try and parse
@@ -18,3 +19,25 @@ var parseGeoJson = function (geojson) {
 
 module.exports.parseGeoJson = parseGeoJson;
 
+
+var areaNotLarge = function (geojson) {
+  if (validator.isNumeric(process.env.AREA_LIMIT)) {
+    var areaLimit = parseInt(process.env.AREA_LIMIT);
+  }
+  else {
+    var areaLimit = 500000;
+  }
+
+  // calculate area
+  var area = turfArea(geojson) / 1000000;
+
+  // If it is smaller than Nigeria use geohash
+  if (area < areaLimit) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+module.exports.areaNotLarge = areaNotLarge;
