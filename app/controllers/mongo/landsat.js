c871cdba86ae6148f0ae584586995dec7977aeb8
Scisco <scisco7@gmail.com>
2015-09-04 15:43:12 -0400

    sort by acquisitionDate DESC

--- app/controllers/mongo/landsat.js
+++ app/controllers/mongo/landsat.js
@@ -38,15 +38,15 @@ module.exports = function (params, request, cb) {
       sceneCenterLongitude: 1,
       cloudCover: 1,
       boundingBox: 1
-    }
+    };
   }
 
   var db = request.server.plugins['hapi-mongodb'].db;
   var collection = db.collection('landsats');
-  var query = collection.find(q, {skip: skip, limit: request.limit, fields: fields});
+  var query = collection.find(q, {skip: skip, limit: request.limit, fields: fields}).sort({acquisitionDate: -1});
   query.toArray(function (err, records) {
     if (err) return cb(err);
-    collection.find(q).count(function (err, count){
+    collection.find(q).count(function (err, count) {
       cb(err, records, count);
     });
   });
