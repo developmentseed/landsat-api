45dd0985f252acacfd59a9b70bfd16f00f87c7a7
Scisco <scisco7@gmail.com>
2016-05-09 10:56:35 -0400

    fix if condition

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e # halt script on error
 
 # If this is the deploy branch, push it up to gh-pages
-if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
+if [ "$TRAVIS_BRANCH" == "v0_1" ]; then
   cd app
   git init
   git config user.name "Travis-CI"
