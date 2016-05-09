52607743753ab396175bca8265030a174e5ba3f6
Scisco <scisco7@gmail.com>
2016-05-09 10:49:47 -0400

    deployment to heroku

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+# If this is the deploy branch, push it up to gh-pages
+if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
+  cd app
+  git init
+  git config user.name "Travis-CI"
+  git config user.email "travis@somewhere.com"
+  git add .
+  git commit -m "CI deploy to gh-pages"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"
+else
+  echo "Not a publishable branch so we're all done here"
+fi
